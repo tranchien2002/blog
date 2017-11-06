d953a604bf7c7c78a36c16a8558abc61e6487fd3
tmc <chie.tm2002@gmail.com>
2017-11-06 17:10:03 +0700

    fix box input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+  	@user = User.new(user_param)
+  	if(@user.save)
+  		flash[:info] = "created"
+  	else
+  		render "new"
+  	end
+  			
+  end
+
+  private
+
+  	def user_param
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)  		
+  	end
 end
