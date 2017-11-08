0425f81206892229e8487d6d8fff8ef40752c232
tranchien2002 <chie.tm2002@gmail.com>
2017-11-08 19:12:46 +0700

    test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,21 @@
 class CommentsController < ApplicationController
 	def create
-		@post = Post.find(params[:post_id])
-
-		@comment = @post.comments.build(comment_params)
+		@comment = Comment.new(comment_params)
 		@comment.user_id = current_user.id
+
 		if @comment.save
-			# render json:{
-			# 	comment_data: render_to_string(@comment)
-			# }
-			redirect_to @post
+			render json: {
+				comment_data: render_to_string(@comment)
+			}
+			# redirect_to @post
 		else
 			flash[:success] = @comment
 			render "static_pages/home"
 		end
-
 	end
-
 
 	private
 		def comment_params
-			params.require(:comment).permit(:content)
+			params.require(:comment).permit(:content, :post_id)
 		end
 end
